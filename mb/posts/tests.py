06ff603bbb670053d460bfcde3030dfaedb9cb9c
chabtj <tejasvichabbra@yahoo.in>
2020-06-05 20:16:54 +0530

    tests

--- mb/posts/tests.py
+++ mb/posts/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
-
+from .models import Message
 # Create your tests here.
+class MessageModelTest(TestCase):
+	def setUp(self):
+		Message.objects.create(message="this is a test")
+	def test_message_conten(self):
+		x=Message.objects.get(id=1)
+		expected=f'{x.message}'
+		self.assertEqual('this is a test',expected)
+
+class ChatviewViewTest(TestCase):
+	def setUp(self):
+		Message.objects.create(message='this is a test')
+
+
+	def check_home_page_code(self):
+		response=self.client.get('/home')
+		self.assertEqual(response.status_code,200)
+
